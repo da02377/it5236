29f3bfc63b56fea9f4167e7d660fee9e210fe004
da02377 <71030970+da02377@users.noreply.github.com>
2020-09-15 19:42:38 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,37 @@
   <meta name="author" content="Derrick Abrams">
 </head>
 <body>
-  <p></p>
+  <div style="padding-left:40px">
+        <h2>Calculator</h2>
+        <form method="post" action="">
+            
+            Answer: <input type="text" name="value"> <br> <br> 
+            <div>
+                <input type="submit" value="AC" name="button" style="padding-left: 5px">
+                <input type="submit" value="x^y" name="button" style="padding-left: 5px">
+                <input type="submit" value="sqrt" name="button" style="padding-left: 5px"> 
+                <input type="submit" value="%" name="button" style="padding-left: 5px">
+                <br>
+                <input type="submit" value="7" name="button" style="padding-left: 18px">
+                <input type="submit" value="8" name="button" style="padding-left: 18px">
+                <input type="submit" value="9" name="button" style="padding-left: 18px">
+                <input type="submit" value="*" name="button" style="padding-left: 18px">
+                <br>
+                <input type="submit" value="4" name="button" style="padding-left: 18px">
+                <input type="submit" value="5" name="button" style="padding-left: 18px">
+                <input type="submit" value="6" name="button" style="padding-left: 18px">
+                <input type="submit" value="+" name="button" style="padding-left: 18px"> 
+                <br>
+                <input type="submit" value="1" name="button" style="padding-left: 18px"> 
+                <input type="submit" value="2" name="button" style="padding-left: 18px">
+                <input type="submit" value="3" name="button" style="padding-left: 18px">
+                <input type="submit" value="-" name="button" style="padding-left: 18px">
+                <br>
+                <input type="submit" value="0" name="button" style="padding-left: 58px">
+                <input type="submit" value="." name="button" style="padding-left: 18px">
+                <input type="submit" value="=" name="button" style="padding-left: 18px">
+            </div>           
+        </form>     
+    </div>
 </body>
 </html>
