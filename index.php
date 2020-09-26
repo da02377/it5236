abf24be3cac2d3653c5b3d2b8b5c83aef866fe25
da02377 <71030970+da02377@users.noreply.github.com>
2020-09-26 17:53:44 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,50 +3,47 @@
 <head>
   <meta charset="UTF-8">
   <meta name="author" content="Derrick Abrams">
-  <link href="frameworks/css/css.css rel="stylesheet">
+  <link href="frameworks/css/css.css" rel="stylesheet">
   <script src="js/javascript.js"></script>
 </head>
 <body>
-  <div>
-        <h2>Calculator</h2>
-        
-        <table border="1"> 
-         <tr> 
-            <td colspan="4"><input type="text" id="answer"/></td> 
-         </tr>
-         <tr>
-            <td><input type="button" value="AC" onclick="reset()" /></td>
-            <td><input type="button" value="x^y" onclick="dis('**')" /></td>
-            <td><input type="button" value="sqrt" onclick="sqroot()" /></td> 
-            <td><input type="button" value="/" onclick="dis('/')" /></td>
-         </tr>
-         <tr> 
+  <div class = "calculator">
+        <h2>Free Calculator App</h2>        
+        <form name ="calculator">
+            <input type="text" id="answer"/> 
+            <br>
+            
+            <input type="button" value="AC" onclick="reset()" />
+            <input type="button" value="x^y" onclick="dis('**')" />
+            <input type="button" value="sqrt" onclick="sqroot()" /> 
+            <input type="button" value="/" onclick="dis('/')" />
+            <br>
+         
             <!-- create button and assign value to each button -->
             <!-- dis("1") will call function dis to display value -->
-            <td><input type="button" value="7" onclick="dis('7')"/></td> 
-            <td><input type="button" value="8" onclick="dis('8')"/></td> 
-            <td><input type="button" value="9" onclick="dis('9')"/></td> 
-            <td><input type="button" value="*" onclick="dis('*')"/></td> 
-         </tr> 
-         <tr> 
-            <td><input type="button" value="4" onclick="dis('4')"/> </td> 
-            <td><input type="button" value="5" onclick="dis('5')"/> </td> 
-            <td><input type="button" value="6" onclick="dis('6')"/> </td> 
-            <td><input type="button" value="+" onclick="dis('+')"/> </td> 
-         </tr> 
-         <tr> 
-            <td><input type="button" value="1" onclick="dis('1')"/> </td> 
-            <td><input type="button" value="2" onclick="dis('2')"/> </td> 
-            <td><input type="button" value="3" onclick="dis('3')"/> </td> 
-            <td><input type="button" value="-" onclick="dis('-')"/> </td> 
-         </tr> 
-         <tr> 
-            <td colspan='2'><input type="button" value="0" onclick="dis('0')"/> </td> 
-            <td><input type="button" value="." onclick="dis('.')"/> </td> 
+            <input type="button" value="7" onclick="dis('7')"/> 
+            <input type="button" value="8" onclick="dis('8')"/> 
+            <input type="button" value="9" onclick="dis('9')"/>
+            <input type="button" value="*" onclick="dis('*')"/>
+            <br>
+          
+            <input type="button" value="4" onclick="dis('4')"/>
+            <input type="button" value="5" onclick="dis('5')"/>
+            <input type="button" value="6" onclick="dis('6')"/>
+            <input type="button" value="+" onclick="dis('+')"/>
+            <br>
+         
+            <input type="button" value="1" onclick="dis('1')"/> 
+            <input type="button" value="2" onclick="dis('2')"/>> 
+            <input type="button" value="3" onclick="dis('3')"/> 
+            <input type="button" value="-" onclick="dis('-')"/>
+            <br>
+         
+            <input type="button" value="0" onclick="dis('0')"/>
+            <input type="button" value="." onclick="dis('.')"/>
             <!-- solve function call function solve to evaluate value -->
-            <td><input type="button" value="=" onclick="solve()"/> </td> 
-         </tr> 
-      </table>   
+            <input type="button" value="=" onclick="solve()"/ style="width:44%"> 
+            <br>  
     </div>
     <script src="js/javascript.js"></script>
 </body>
