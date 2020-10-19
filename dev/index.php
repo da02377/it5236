60fad6355fd7ea172850b093d0a00ecefcb06be0
da02377 <71030970+da02377@users.noreply.github.com>
2020-10-18 20:24:52 -0400

    Update index.php

--- dev/index.php
+++ dev/index.php
@@ -1,22 +1,24 @@
 <?php
-$servername = "localhost";
-$username = "phpmyadmin";
-$password = "irmaabra35";
-$database = "mysql";
+    $servername = "localhost";
+    $username = "phpmyadmin";
+    $password = "irmaabra35";
+    $database = "mysql";
+    $conn = new mysqli($servername,$username,$password,$database);
+    
+    $description = "Page Accessed";
+    $time = date("Y-m-d H:i:s");
+    $useragent = $_SERVER["HTTP_USER_AGENT"];
+    $remoteip = $_SERVER["REMOTE_ADDR"];
+    $hostip = $_SERVER["HTTP_HOST"];
 
-$conn = new mysqli($servername,$username,$password,$database);
-
-$ip=$_SERVER['REMOTE_ADDR'];
-
-$sql = "INSERT INTO access_log (time,ip) VALUES (current_timestamp(),'$ip')";
-
-if ($conn->query($sql) === TRUE) {
-  echo "New log record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
+    $sql = "INSERT INTO access_log (description, time, useragent, remoteip, hostip) "
+        . "VALUES ('$description','$time','$useragent','$remoteip','$hostip')";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
 
 ?>
 
@@ -85,57 +87,3 @@ $conn->close();
     <script src="js/javascript.js"></script>
 </body>
 </html>
-
-<!--<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="author" content="Derrick Abrams">
-  <link href="frameworks/css/css.css" rel="stylesheet">
-  <title>Welcome to the Calculator App</title>
-  <script src="js/javascript.js"></script>
-</head>
-<body>
-  <div class = "calculator">
-        <h2>Free Calculator App</h2>        
-        <h2>Calculator App - Derrick Abrams</h2>        
-        <form name ="calculator">
-            <input type="text" id="answer"/> 
-            <input readonly type="text" id="answer"/> 
-            <br>
-            
-            <input type="button" value="AC" onclick="reset()" />
-            <input type="button" value="x^y" onclick="dis('**')" />
-            <input type="button" value="sqrt" onclick="sqroot()" /> 
-            <input type="button" value="/" onclick="dis('/')" />
-            <br>
-         
-             create button and assign value to each button 
-             dis("1") will call function dis to display value 
-            <input type="button" value="7" onclick="dis('7')"/> 
-            <input type="button" value="8" onclick="dis('8')"/> 
-            <input type="button" value="9" onclick="dis('9')"/>
-            <input type="button" value="*" onclick="dis('*')"/>
-            <br>
-          
-            <input type="button" value="4" onclick="dis('4')"/>
-            <input type="button" value="5" onclick="dis('5')"/>
-            <input type="button" value="6" onclick="dis('6')"/>
-            <input type="button" value="+" onclick="dis('+')"/>
-            <br>
-         
-            <input type="button" value="1" onclick="dis('1')"/> 
-            <input type="button" value="2" onclick="dis('2')"/>
-            <input type="button" value="3" onclick="dis('3')"/> 
-            <input type="button" value="-" onclick="dis('-')"/>
-            <br>
-         
-            <input type="button" value="0" onclick="dis('0')"/>
-            <input type="button" value="." onclick="dis('.')"/>
-             solve function call function solve to evaluate value 
-            <input type="button" value="=" onclick="solve()"/ style="width:44%"> 
-            <br>  
-    </div>
-    <script src="js/javascript.js"></script>
-</body>
-</html>-->
