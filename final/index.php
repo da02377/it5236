bb8f896141fb7e4a614f09df1104c0f6c5f3ea1d
da02377 <71030970+da02377@users.noreply.github.com>
2020-12-06 14:10:54 -0500

    Update index.php

--- final/index.php
+++ final/index.php
@@ -4,10 +4,22 @@
   <meta charset="UTF-8">
   <meta name="author" content="Derrick Abrams">
   <title>The Final Project</title>
+  <style>
+  	h1,h4 {
+		text-align: center;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+	body {
+		display: block;
+		text-align: center;
+	}
+  </style>
 </head>
 <body>
-     <h1>Celsius to Fahrenheit Converter</h1>   
-     <p>By Derrick Abrams</p>
+	<h1>Celsius to Fahrenheit Converter</h1>   
+	<h4>By Derrick Abrams</h4>
+
+	<br><br>	
 		
 	<label>Degrees Celsius: </label>
 	<input type="number" id="celsius" name="input" placeholder="0" pattern="[0-9]+">
@@ -15,14 +27,18 @@
 	<br>
 	<script>
 	function solve() {
-		cels = document.getElementById("celsius").value;
-		toFahr = (cels * (9/5)) + 32;
-		document.getElementById("answer").innerHTML = toFahr + "&#x2109;" ;
-	}
+		var cels = document.getElementById("celsius").value;
+		// Formula from https://www.rapidtables.com/convert/temperature/how-celsius-to-fahrenheit.html
+		var toFahr = (cels * (9/5)) + 32;
+		document.getElementById("answer").innerHTML = toFahr + "&#x2109;";
+	}	
+
 	</script>
 	
 	<br>	
 	<input type="button" value="SOLVE" onclick="solve()"/>
+
+	<br><br>
 
 	<h2 id="answer"></h2>
 		
